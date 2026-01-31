c3007d3ef491be7570d04e7d5859931beb8ea18c
Avery Rair <drslug7@gmail.com>
2026-01-31 13:40:43 -0500

    small prefab changes

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -14,5 +14,18 @@ public class Ghost : MonoBehaviour
         Ed
     }
 
-    public GhostName Name {get; set;}
+    [SerializeField]
+    private GhostName g_name;
+    public GhostName Name
+    {
+        get
+        {
+            return g_name;
+        }
+        set
+        {
+            if (g_name == value) return;
+            g_name = value;
+        }
+    }
 }
